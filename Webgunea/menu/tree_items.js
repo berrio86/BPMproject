9bcb9dc32cd8c613a9ab7c061eae1656bbce562e
Inaki Berriotxoa <berriotxoa86@gmail.com>
2017-04-03 17:00:57 +0200

    webgune erroa gehituta

---
+++ Webgunea/menu/tree_items.js
@@ -0,0 +1,202 @@
+/* --- menu items --- */
+var TREE_ITEMS = [
+	['Proyecto XX', null,
+			['Portada', '../Proyecto/0_1_portada.doc'],
+			['Indice general', '../Proyecto/0_2_indice.doc'],
+			['Presentación', '../Proyecto/0_3_presentacion.htm'],
+			['Búsqueda', '../main/home_page.htm#search'],
+			
+			['Memoria','../Proyecto/Memoria/1_0_memoria.htm',
+				['Resumen', '../Proyecto/Memoria/1_1_resumen.doc'],
+				['Introducción', '../Proyecto/Memoria/1_2_introduccion.doc'],
+				['Objeto', '../Proyecto/Memoria/1_3_objeto.doc'],
+				['Antecedentes', '../Proyecto/Memoria/1_4_antecedentes.doc'],
+				['Situación actual', '../Proyecto/Memoria/1_5_situacion-actual.doc'],
+			        ['Normas y referencias', '../Proyecto/Memoria/1_6_normas-referencias.doc',
+
+				         ['<A HREF="../Proyecto/Memoria/1_6_normas-referencias.doc#Disposiciones_legales_y_normas_aplicadas" target="principal">Disposiciones legales y normas aplicadas</A>'],
+				         ['Métodos y recursos de desarrollo utilizados', '../Proyecto/Memoria/1_6_2_metodos-recursos-desarrollo.doc'],
+				         ['Recursos para soporte a las estimaciones', '../Proyecto/Memoria/1_6_3_recursos-estimaciones.doc'],
+				         ['Plan de gestión de calidad aplicado', '../Proyecto/Memoria/1_6_4_gestion-calidad.doc'],
+				         ['Otras referencias', '../Proyecto/Memoria/1_6_5_otras-referencias.doc'],
+				         ['Bibliografía', '../Proyecto/Memoria/1_6_6_bibliografia.doc']
+                                ],
+				['Definiciones y abreviaturas', '../Proyecto/Memoria/1_7_def-abreviaturas.doc'],
+				['Requisitos iniciales', '../Proyecto/Memoria/1_8_requisitos-iniciales.doc'],
+				['Alcance', '../Proyecto/Memoria/1_9_alcance.doc'],
+				['Hipótesis y restricciones', '../Proyecto/Memoria/1_10_hipotesis-restricciones.doc'],
+				['Alternativas y viabilidad', '../Proyecto/Memoria/1_11_alternativas-viabilidad.doc',
+				         ['Estudio de alternativas', '../Proyecto/Memoria/1_11_1_alternativas.doc'],
+				         ['Estudio de  viabilidad', '../Proyecto/Memoria/1_11_2_viabilidad.doc']
+                                ],
+				['Solución propuesta', '../Proyecto/Memoria/1_12_solucion-propuesta.doc',
+				         ['Descripción del sistema', '../Proyecto/Memoria/1_12_1_descripcion.doc'],
+				         ['Esquema DAFO', '../Proyecto/Memoria/1_12_2_DAFO.doc']
+                                ],
+				['Previsiones para la construción', '../Proyecto/Memoria/1_13_previsiones-construccion.doc',
+				         ['Análisis de riesgos', '../Proyecto/Memoria/1_13_1_analisis-riesgos.doc',
+				         	['Riesgos tecnológicos', '../Proyecto/Memoria/1_13_1_1_riesgos-tecnológicos.doc'],
+				         	['Otros riesgos', '../Proyecto/Memoria/1_13_1_2_otros-riesgos.doc']
+                                         ],
+				         ['Organización y gestión ', '../Proyecto/Memoria/1_13_2_organizacion-gestion.doc'],
+				         ['Planificación temporal', '../Proyecto/Memoria/1_13_3_planificacion-temporal.doc'],
+				         ['Presupuesto', '../Proyecto/Memoria/1_13_4_presupuesto.doc']
+                                ]
+
+                         ],
+                         
+                         
+ 
+
+
+
+
+			['ANEXOS: Detalles técnicos de la solución','../Proyecto/Memoria-Anexos/2_0_anexos-memoria.doc',
+			
+				['Documentación de entrada', '../Proyecto/Memoria-Anexos/2_1_documentacion-entrada.doc'],
+				
+				['Especificación del sistema','../Proyecto/Memoria-Anexos/2_2_especificacion-sistema.doc',
+					['Generalidades', '../Proyecto/Memoria-Anexos/2_2_1_generalidades.doc'],
+					['Contenido', '../Proyecto/Memoria-Anexos/2_2_2_contenido.doc'],
+					['Requisitos', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
+				         	['Glosario', '../Proyecto/Memoria-Anexos/2_2_3_1_glosario.doc'],
+				         	['Visión', '../Proyecto/Memoria-Anexos/2_2_3_2_especificacion-sistema.doc'],
+				         	['Solicitudes de los interesados', '../Proyecto/Memoria-Anexos/2_2_3_3_especificacion-sistema.doc'],
+				         	['Especificaciones suplementarias', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc'],
+				         	['Modelo de casos de uso', '../Proyecto/Memoria-Anexos/2_2_3_5_modelo-casos-uso.htm'],
+				         	['Guión gráfico', '../Proyecto/Memoria-Anexos/2_2_3_6_guion-grafico.doc'],
+				         	['Requisitos de software', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
+				         	['Especificación de requisitos de software', '../Proyecto/Memoria-Anexos/2_2_3_8_especificacion-requisitos.doc']
+                                        ]
+				],
+				
+
+			
+				['Guías para el desarrollo', '../Proyecto/Memoria-Anexos/2_3_guias-desarrollo.doc',
+				         ['Infraestructura de desarrollo', '../Proyecto/Memoria-Anexos/2_3_1_infraestructura-desarrollo.doc'],
+				         ['Proceso de desarrollo', '../Proyecto/Memoria-Anexos/2_3_2_proceso-desarrollo.doc'],
+					 ['Caso del desarrollo', '../Proyecto/Memoria-Anexos/2_3_3_caso-desarrollo.doc']
+                                ],
+			
+                  
+				['Análisis y diseño del sistema', '../Proyecto/Memoria-Anexos/2_4_analisis-diseño.doc',
+				         ['Arquitectura de prueba de concepto', '../Proyecto/Memoria-Anexos/2_4_1_arquitectura-prueba-concepto.doc'],
+				         ['Arquitectura de referencia', '../Proyecto/Memoria-Anexos/2_4_2_arquitectura-referencia.doc'],
+				         ['Documento de arquitectura de software', '../Proyecto/Memoria-Anexos/2_4_3_arquitectura-software.doc'],
+				         ['Modelo de análisis', '../Proyecto/Memoria-Anexos/2_4_4_modelo-analisis.doc'],
+				         ['Prototipo de interfaz de usuario', '../Proyecto/Memoria-Anexos/2_4_5_interfaz-usuario.doc'],
+				         ['Mapa de navegación', '../Proyecto/Memoria-Anexos/2_4_6_mapa-navegacion.doc'],
+				         ['Modelo de diseño', '../Proyecto/Memoria-Anexos/2_4_7_modelo-diseño.doc'],
+				         ['Modelo de datos', '../Proyecto/Memoria-Anexos/2_4_8_modelo-datos.doc'],
+				         ['Modelo de despliegue', '../Proyecto/Memoria-Anexos/2_4_9_modelo-despliegue.doc']
+                                ],
+
+				['Implementación', '../Proyecto/Memoria-Anexos/2_5_implementacion.doc',
+				         ['Modelo de implementación', '../Proyecto/Memoria-Anexos/2_5_1_modelo-implementacion.doc'],
+				         ['Plan de integración', '../Proyecto/Memoria-Anexos/2_5_2_plan-integracion.doc'],
+					 ['Compilación', '../Proyecto/Memoria-Anexos/2_5_3_compilacion.doc',
+				         	['Código fuente', '../Proyecto/Memoria-Anexos/2_5_3_1_codigo-fuente.doc'],
+				         	['Proceso de compilación', '../Proyecto/Memoria-Anexos/2_5_3_2_proceso-compilacion.doc'],
+				         	['Ejecutables', '../Proyecto/Memoria-Anexos/2_5_3_3_ejecutables.doc'],
+				         	['Documentación', '../Proyecto/Memoria-Anexos/2_5_3_4_documentación.doc']
+                                         ],
+					 ['Prueba de desarrollador', '../Proyecto/Memoria-Anexos/2_5_4_prueba-desarrollador.doc',
+				         	['Pruebas de unidad', '../Proyecto/Memoria-Anexos/2_5_4_1_pruebas-unidad.doc'],
+				         	['Pruebas de integración', '../Proyecto/Memoria-Anexos/2_5_4_2_pruebas-integracion.doc'],
+				         	['Proceso del sistema', '../Proyecto/Memoria-Anexos/2_5_4_3_proceso-sistema.doc']
+                                         ]
+                                ],
+				['Prueba', '../Proyecto/Memoria-Anexos/2_6_prueba.doc',
+				         ['Lista de ideas de prueba', '../Proyecto/Memoria-Anexos/2_6_1_lista-ideas-prueba.doc'],
+				         ['Conjunto de aplicaciones de prueba', '../Proyecto/Memoria-Anexos/2_6_2_aplicaciones-prueba.doc'],
+				         ['Estrategia de prueba', '../Proyecto/Memoria-Anexos/2_6_3_estrategia-prueba.doc'],
+				         ['Registro de prueba', '../Proyecto/Memoria-Anexos/2_6_4_registro-prueba.doc'],
+				         ['Resultados de la prueba', '../Proyecto/Memoria-Anexos/2_6_5_resultados-prueba.doc'],
+				         ['Resumen de evaluación de pruebas', '../Proyecto/Memoria-Anexos/2_6_6_resumen-evaluacion-pruebas.doc']
+                                ],
+
+
+				['Configuración y gestión de cambios', '../Proyecto/Memoria-Anexos/2_7_configuracion-gestion-cambios.doc',
+				         ['Solicitud de cambio', '../Proyecto/Memoria-Anexos/2_7_1_solicitudes-cambio.doc']
+				         ['Repositorio del proyecto', '../Proyecto/Memoria-Anexos/2_7_2_repositorio-proyecto.doc'],
+                                ],
+                                
+
+				['Planes de gestión de riesgos', '../Proyecto/Memoria-Anexos/2_8_planes-gestion-riesgos.doc'],
+
+				['Planes de gestión del proyecto', '../Proyecto/Memoria-Anexos/2_9_planes-gestion-proyecto.doc'],
+
+				['Plan de seguridad', '../Proyecto/Memoria-Anexos/2_10_seguridad.doc'],
+				
+				['Guías para la construcción', '../Proyecto/Memoria-Anexos/2_11_guias-construccion.doc',
+				         ['Conceptos', '../Proyecto/Memoria-Anexos/2_11_1_conceptos.doc'],
+				         ['Directrices', '../Proyecto/Memoria-Anexos/2_11_2_directrices.doc'],
+				         ['Materiales de soporte', '../Proyecto/Memoria-Anexos/2_11_3_materiales-soporte.doc'],
+				         ['Documentación', '../Proyecto/Memoria-Anexos/2_11_4_documentacion.doc'],
+				         ['Plantillas', '../Proyecto/Memoria-Anexos/2_11_5_plantillas.doc'],
+				         ['Listas de comprobación', '../Proyecto/Memoria-Anexos/2_11_6_lista-comprobacion.doc'],
+				         ['Ejemplos', '../Proyecto/Memoria-Anexos/2_11_7_ejemplos.doc'],
+				         ['Guías de herramientas', '../Proyecto/Memoria-Anexos/2_11_8_guias-herramientas.doc']
+                                ],
+
+
+				['Estudios de terceros','../Proyecto/Memoria-Anexos/2_12_estudios-terceros.doc',
+					['Generalidades', '../Proyecto/Memoria-Anexos/2_12_1_generalidades.doc'],
+					['Contenido', '../Proyecto/Memoria-Anexos/2_12_2_contenido.doc']
+				],
+
+				['Presupuesto','../Proyecto/Memoria-Anexos/2_13_presupuesto.doc',
+					['Generalidades', '../Proyecto/Memoria-Anexos/2_13_1_generalidades.doc'],
+					['Estimación del tamaño y esfuerzo', '../Proyecto/Memoria-Anexos/2_13_2_estimacion-tamaño-esfuerzo.doc'],
+					['Contenido', '../Proyecto/Memoria-Anexos/2_13_3_contenido.doc']
+
+				]
+
+			],
+
+
+			
+			['ELABORACIÓN DEL PROYECTO (interno)','../Proyecto/Memoria-Elaboracion/5_0_elaboracion-proyecto.htm',
+				['Memoria', '../Proyecto/Memoria-Elaboracion/5_0_memeoria-elaboracion.doc',
+					['Portada', '../Proyecto/Memoria-Elaboracion/5_1_portada.doc'],
+					['Introducción', '../Proyecto/Memoria-Elaboracion/5_2_introduccion.doc'],
+					['Objeto', '../Proyecto/Memoria-Elaboracion/5_3_objeto.doc'],
+					['Antecedentes', '../Proyecto/Memoria-Elaboracion/5_4_antecedentes.doc'],
+					['Métodos y recursos', '../Proyecto/Memoria-Elaboracion/5_5_metodos-recursos.doc',
+						['Miembros del proyecto', '../Proyecto/Memoria-Elaboracion/5_5_1_miembros.doc'],
+						['Bileretako aktak', '../Proyecto/Memoria-Elaboracion/5_5_2_actas.doc']
+				        ],
+				        ['Resultados', '../Proyecto/Memoria-Elaboracion/5_6_resultados.doc']
+		  		],
+					
+				
+                                ['Gestión del proyecto', '../Proyecto/Memoria-Elaboracion/5_7_gestion-proyecto.doc',
+					 ['Caso del negocio', '../Proyecto/Memoria-Elaboracion/5_7_1_caso-negocio.doc'],
+					 ['Lista de riesgos', '../Proyecto/Memoria-Elaboracion/5_7_2_lista-riesgos.doc'],
+					 ['Pedido de trabajo', '../Proyecto/Memoria-Elaboracion/5_7_3_pedido-trabajo.doc'],
+					 ['Plan del proyecto', '../Proyecto/Memoria-Elaboracion/5_7_4_plan-proyecto.doc'],
+					 ['Plan de desarrollo de software', '../Proyecto/Memoria-Elaboracion/5_7_5_plan-desarrollo-software.doc'],
+					 ['Planes de iteración', '../Proyecto/Memoria-Elaboracion/5_7_6_planes-iteracion.doc'],
+					 ['Registro de revisión', '../Proyecto/Memoria-Elaboracion/5_7_7_registro-revision.doc'],
+					 ['Valoración de estado', '../Proyecto/Memoria-Elaboracion/5_7_8_valoracion-estado.doc'],
+					 ['Valoración de iteración', '../Proyecto/Memoria-Elaboracion/5_7_9_valoracion-iteracion.doc']
+				],
+			
+
+				['Presupuesto','../Proyecto/Memoria-Elaboracion/5_8_presupuesto.doc',
+					['Generalidades', '../Proyecto/Memoria-Elaboracion/5_8_1_generalidades.doc'],
+					['Contenido', '../Proyecto/Memoria-Elaboracion/5_8_2_contenidos.doc']
+				]
+            
+
+			]                         
+
+
+
+
+			
+	],			
+			
+];
+
+ 
