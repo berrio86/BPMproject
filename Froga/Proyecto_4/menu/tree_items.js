1affc4b06145e255c2c88f0c17fc3b69b2f89e3b
Julen Ferrero <julenferre@gmail.com>
2017-04-27 18:38:05 +0200

    1 fasea bukatuta xD

---
+++ Froga/Proyecto_4/menu/tree_items.js
@@ -0,0 +1,115 @@
+/* --- menu items --- */
+var TREE_ITEMS = [
+	['Proyecto XX', null,
+
+	['Indice general', '',
+	['Portada', '../Proyecto/0_1_portada.doc'],
+	['Indice general', '../Proyecto/0_2_indice.doc'],
+	['Presentacion', '../Proyecto/0_3_presentacion.htm'],
+	['Busqueda', '../main/home_page.htm#search'],
+	 ],
+
+
+			['Memoria','../Proyecto/Memoria/1_0_memoria.htm',
+				['Hojas de identificacion', '../Proyecto/Memoria/1_1_resumen.doc'],
+				['Introduccion', '../Proyecto/Memoria/1_2_introduccion.doc'],
+				['Objeto', '../Proyecto/Memoria/1_3_objeto.doc'],
+				['Antecedentes', '../Proyecto/Memoria/1_4_antecedentes.doc'],
+				['Descripcion de la situacion actual', '../Proyecto/Memoria/1_5_situacion-actual.doc'],
+			        ['Normas y referencias', '../Proyecto/Memoria/1_6_normas-referencias.doc',
+
+				         ['<A HREF="../Proyecto/Memoria/1_6_normas-referencias.doc#Disposiciones_legales_y_normas_aplicadas" target="principal">Disposiciones legales y normas aplicadas</A>'],
+								 ['Bibliografia', '../Proyecto/Memoria/1_6_6_bibliografia.doc'],
+								 ['Metodos, herramientas, modelos, metricas y prototipos', '../Proyecto/Memoria/1_6_2_metodos-recursos-desarrollo.doc'],
+				         ['Plan de gestion de la calidad aplicado durante la redaccion del proyecto', '../Proyecto/Memoria/1_6_4_gestion-calidad.doc'],
+				         ['Otras referencias', '../Proyecto/Memoria/1_6_5_otras-referencias.doc']
+                                ],
+				['Definiciones y abreviaturas', '../Proyecto/Memoria/1_7_def-abreviaturas.doc'],
+				['Requisitos iniciales', '../Proyecto/Memoria/1_8_requisitos-iniciales.doc'],
+				['Alcance', '../Proyecto/Memoria/1_9_alcance.doc'],
+				['Hipotesis y restricciones', '../Proyecto/Memoria/1_10_hipotesis-restricciones.doc'],
+				['Estudio de alternativas y viabilidad', '../Proyecto/Memoria/1_11_alternativas-viabilidad.doc'
+					],
+				['Descripcion del sistema propuesto', '../Proyecto/Memoria/Descripcion-del-sistema-propuesto.doc'],
+				['Analisis de riesgos', '../Proyecto/Memoria/Analisis-riesgos.doc'],
+				['Organizacion y gestion del proyecto', '../Proyecto/Memoria/Organizacion-gestion-proyecto.doc'],
+				['Planificacion temporal', '../Proyecto/Memoria/Planificacion-temporal.doc'],
+				['Presupuesto', '../Proyecto/Memoria/Presupuesto.doc']
+
+
+
+			],
+
+
+			['ANEXOS: Detalles tecnicos de la solucion','../Proyecto/Memoria-Anexos/2_0_anexos-memoria.doc',
+
+			['Analisis y diseño del sistema', '../Proyecto/Memoria-Anexos/2_4_analisis-diseño.doc',
+			['Arquitectura', '',
+			['Cuaderno de arquitectura', '../Proyecto/openUp/architecture_notebook_tpl.dot'],
+				],
+			['Desarrolo', '',
+			['Construccion', '../Proyecto/openUp/add/Build.docx'],
+			['Diseño', '../Proyecto/openUp/design.docx'],
+			['Pruebas de desarrolo', '../Proyecto/openUp/add/Developertest.docx'],
+			['Implementacion', '../Proyecto/openUp/add/Implementation.docx'],
+					],
+					['Pruebas', '',
+					['Casos de prueba', '../Proyecto/openUp/test_cases.dot'],
+					['Registro de pruebas', '../Proyecto/openUp/add/TestLog.docx'],
+					['Script de pruebas', '../Proyecto/openUp/test_script_tpl.dot'],
+						],
+															],
+
+
+									['Estimacion de tamaño y esfuerzo', '',
+									['Metricas', '../Proyecto/openUp/add/Metricas.docx'],
+									['Estimacion del tamaño y esfuerzo', '../Proyecto/Memoria-Anexos/2_13_2_estimacion-tamano-esfuerzo.docx'],
+
+
+								],
+
+				['Planes de gestion y ejecucion', '',
+				['Direccion de proyecto', '',
+				['Plan de iteracion', '../Proyecto/openUp/iteration_plan_tpl.dot'],
+				['Plan de proyecto', '../Proyecto/openUp/project_plan_tpl.dot'],
+				['Lista de riesgos', '../Proyecto/openUp/risk_list_tpl.xls'],
+				['Lista de tareas', '../Proyecto/openUp/work_items_list_tpl.xls'],
+					],
+												 ],
+
+								['Seguridad', '',
+								['Plan de seguridad', '../Proyecto/Memoria-Anexos/2_10_seguridad.doc'],
+
+								 ],
+
+			],
+			['Requisitos del sistema', '',
+		  ['Requisitos', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
+		 				['Glosario', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_1_glosario.doc'],
+		 				['Vision', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_2_especificacion-sistema.doc'],
+		 				['Solicitudes de los interesados', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_3_especificacion-sistema.doc'],
+		 				['Especificaciones suplementarias', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_4_especificacion-sup.doc'],
+		 				['Modelo de casos de uso', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_5_modelo-casos-uso.doc'],
+		 				['Guion grafico', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_6_guion-grafico.doc'],
+		 				['Requisitos de software', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_7_requisitos-spftware.doc'],
+		 				['Especificacion de requisitos de software', '../Proyecto/Memoria-Anexos/requisitos/2_2_3_8_especificacion-requisitos.doc']
+		 															 ]
+		  ],
+			['Presupuesto','../Proyecto/Memoria-Anexos/2_13_presupuesto.doc',
+						['Presupuesto', '../Proyecto/Memoria-Anexos/2_13_presupuesto.doc'],
+
+					],
+					['Estudios con entidad propia', '',
+					['Estudios de terceros','../Proyecto/Memoria-Anexos/2_12_estudios-terceros.doc',
+									 ['Generalidades', '../Proyecto/Memoria-Anexos/2_12_1_generalidades.doc'],
+									 ['Contenido', '../Proyecto/Memoria-Anexos/2_12_2_contenido.doc']
+								 ]
+							 ],
+
+
+
+
+
+	],
+
+];
