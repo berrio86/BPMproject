46e98c5b764aca41f331a3cc5788d7b6427e14ea
Julen Ferrero <julenferre@gmail.com>
2017-05-04 17:31:21 +0200

    2 fasea erdi bukatuta

--- Webgunea/menu/tree_items.js
+++ Webgunea/menu/tree_items.js
@@ -1,102 +1,100 @@
 /* --- menu items --- */
 var TREE_ITEMS = [
-	['Proyecto XX', null,
-			['Azala', '../Proyecto/0_1_portada.doc'],
-			['Aurkibide orokorra', '../Proyecto/0_2_indice.doc'],
+	['Proiektua XX', null,
+			['Azala', '../Proiektua/0_1_azala.odt'],
+			['Aurkibide orokorra', '../Proiektua/0_2_aurkibidea.odt'],
 			
-			['Memoria','../Proyecto/Memoria/1_0_memoria.htm',
-				['Identifikazio orriak', '../Proyecto/Memoria/1_1_resumen.doc'],
-				['Sarrera', '../Proyecto/Memoria/1_2_introduccion.doc'],
-				['Xedea', '../Proyecto/Memoria/1_3_objeto.doc'],
-				['Aurrekariak', '../Proyecto/Memoria/1_4_antecedentes.doc'],
-				['Egungo egoera', '../Proyecto/Memoria/1_5_situacion-actual.doc'],
-			    ['Normak eta erreferentziak', '../Proyecto/Memoria/1_6_normas-referencias.doc',
-                     ['<A HREF="../Proyecto/Memoria/1_6_normas-referencias.doc#Disposiciones_legales_y_normas_aplicadas" target="principal">Xedapen legalak eta aplikatutako normak</A>'],
-                     ['Bibliografia', '../Proyecto/Memoria/1_6_6_bibliografia.doc'],
-                     ['Metodoak, herramintak, ereduak, metrikak eta prototipoak', '../Proyecto/Memoria/1_6_2_metodos-recursos-desarrollo.doc'],
-                     ['Proiektuaren idazketan zehar aplikatutako kalitate kudeaketa plana', '../Proyecto/Memoria/1_6_4_gestion-calidad.doc'],
-                     ['Beste erreferentziak', '../Proyecto/Memoria/1_6_5_otras-referencias.doc']                     
+			['Memoria','../Proiektua/Memoria/1_0_memoria.htm',
+				['Identifikazio orriak', '../Proiektua/Memoria/1_1_identifikazio_orriak.odt'],
+				['Sarrera', '../Proiektua/Memoria/1_2_sarrera.odt'],
+				['Xedea', '../Proiektua/Memoria/1_3_xedea.odt'],
+				['Aurrekariak', '../Proiektua/Memoria/1_4_aurrekariak.odt'],
+				['Egungo egoera', '../Proiektua/Memoria/1_5_egungo_egoera.odt'],
+			    ['Arauak eta erreferentziak', '../Proiektua/Memoria/1_6_arauak_erreferentziak.odt',
+                     ['<A HREF="../Proiektua/Memoria/1_6_arauak_erreferentziak.odt#Disposizio_legalak_eta_aplikatutako_arauak" target="nagusia">Xedapen legalak eta aplikatutako normak</A>'],
+                     ['Bibliografia', '../Proiektua/Memoria/1_6_1_bibliografia.odt'],
+                     ['Metodoak, herramintak, ereduak, metrikak eta prototipoak', '../Proiektua/Memoria/1_6_2_metodoak_tresnak_garapena.odt'],
+                     ['Proiektuaren idazketan zehar aplikatutako kalitate kudeaketa plana', '../Proiektua/Memoria/1_6_3_kalitatearen_kudeaketa.odt'],
+                     ['Beste erreferentziak', '../Proiektua/Memoria/1_6_4_beste_erreferentziak.odt']
                 ],
-				['Definizionak eta laburdurak', '../Proyecto/Memoria/1_7_def-abreviaturas.doc'],
-				['Hasierako betekizunak', '../Proyecto/Memoria/1_8_requisitos-iniciales.doc'],
-				['Norainokoa', '../Proyecto/Memoria/1_9_alcance.doc'],
-				['Hipotesiak eta restrikzioak', '../Proyecto/Memoria/1_10_hipotesis-restricciones.doc'],
-				['Aukeren ikerketa eta egingarritasuna', '../Proyecto/Memoria/1_11_alternativas-viabilidad.doc'],
-				['Proposatutako sistemaren deskribapena', '../Proyecto/Memoria/1_12_solucion-propuesta.doc'],
-                ['Arrisku analisia', '../Proyecto/Memoria/1_13_1_analisis-riesgos.doc'],
-                ['Proiektuaren antolaketa eta kudeaketa', '../Proyecto/Memoria/1_13_2_organizacion-gestion.doc'],
-                ['Denbora planifikazioa', '../Proyecto/Memoria/1_13_3_planificacion-temporal.doc'],
-                ['Aurrekontua', '../Proyecto/Memoria/1_13_4_presupuesto.doc']
+				['Definizionak eta laburdurak', '../Proiektua/Memoria/1_7_def_laburdurak.odt'],
+				['Hasierako betekizunak', '../Proiektua/Memoria/1_8_hasierako_betekizunak.odt'],
+				['Norainokoa', '../Proiektua/Memoria/1_9_norainokoa.odt'],
+				['Hipotesiak eta mugapenak', '../Proiektua/Memoria/1_10_hipotesiak_mugapenak.odt'],
+				['Aukeren ikerketa eta egingarritasuna', '../Proiektua/Memoria/1_11_egingarritasuna.odt'],
+				['Proposatutako sistemaren deskribapena', '../Proiektua/Memoria/1_12_proposatutako_sistema.odt'],
+                ['Arrisku analisia', '../Proiektua/Memoria/1_13_arrisku_analisia.odt'],
+                ['Proiektuaren antolaketa eta kudeaketa', '../Proiektua/Memoria/1_14_antolaketa_kudeaketa.odt'],
+                ['Behin-behineko planifikazioa', '../Proiektua/Memoria/1_15_behin_behineko_planif.odt'],
+                ['Aurrekontua', '../Proiektua/Memoria/1_16_aurrekontua.odt']
             ],
          
 
-			['Eranskinak','../Proyecto/Memoria-Anexos/2_0_anexos-memoria.doc',
+			['Eranskinak','../Proiektua/Memoria-Eranskinak/2_0_memoria_eranskinak.odt',
             
-                ['Sistemaren analisi eta diseinua', '../Proyecto/Memoria-Anexos/2_4_analisis-diseño.doc',
-                     ['Analisia: Sistemaren analisi ereduak', '../Proyecto/Memoria-Anexos/2_4_4_modelo-analisis.doc'],
-                     ['Diseinua: Sistemaren arkitektura', '../Proyecto/Memoria-Anexos/2_4_3_arquitectura-software.doc'],
-                     ['Diseinua: Funtzionalitate eredua', '../Proyecto/Memoria-Anexos/2_4_9_modelo-despliegue.doc'],
-                     ['Diseinua: Interfaze eredua', '../Proyecto/Memoria-Anexos/2_4_7_modelo-diseño.doc'],
-                     ['Diseinua: Datu eredua', '../Proyecto/Memoria-Anexos/2_4_8_modelo-datos.doc']                     
+                ['Sistemaren analisi eta diseinua', '../Proiektua/Memoria-Eranskinak/2_1_analisia_diseinua.odt',
+                    ['Analisia: Sistemaren analisi ereduak', '../Proiektua/Memoria-Eranskinak/2_1_1_analisi_ereduak.odt'],
+                    ['Diseinua: Sistemaren arkitektura', '../Proiektua/Memoria-Eranskinak/2_1_2_arkitektura.odt'],
+                    ['Diseinua: Funtzionalitate eredua', '../Proiektua/Memoria-Eranskinak/2_1_3_funtzionalitate_eredua.odt'],
+                    ['Diseinua: Interfaze eredua', '../Proiektua/Memoria-Eranskinak/2_1_4_interfaze_eredua.odt'],
+                    ['Diseinua: Datu eredua', '../Proiektua/Memoria-Eranskinak/2_1_5_datu_eredua.odt'],
+                    ['Architecture', '../Proiektua/Memoria-Eranskinak/2_1_6_arkitektura.odt',
+                        ['Architecture notebook', '../Proiektua/Memoria-Eranskinak/2_1_6_1_arkit_eskuliburua.odt']
+                        ],
+                    ['Development', '../Proiektua/Memoria-Eranskinak/2_1_7_garapena.odt',
+                        ['Build', '../Proiektua/Memoria-Eranskinak/2_1_7_1_eraiketa.odt'],
+                        ['Design', '../Proiektua/Memoria-Eranskinak/2_1_7_2_diseinua.odt'],
+                        ['Developer Test', '../Proiektua/Memoria-Eranskinak/2_1_7_3_garapen_testak.odt'],
+                        ['Implementation', '../Proiektua/Memoria-Eranskinak/2_1_7_4_inplementazioa.odt']
+                        ],
+                    ['Test', '../Proiektua/Memoria-Eranskinak/2_1_8_testak.odt',
+                        ['Test Case', '../Proiektua/Memoria-Eranskinak/2_1_8_1_test_kasuak.odt'],
+                        ['Test Log', '../Proiektua/Memoria-Eranskinak/2_1_8_2_test_log.odt'],
+                        ['Test Script', '../Proiektua/Memoria-Eranskinak/2_1_8_3_test_script.odt']
+                        ]
                 ],
                 
-                ['Tamaina eta esfortzu estimazioak', '../Proyecto/Memoria-Anexos/2_10_seguridad.doc'],
+                ['Tamaina eta esfortzu estimazioak', '../Proiektua/Memoria-Eranskinak/2_2_tamaina_esfortzu_estimazioak.odt'],
                 
-                ['Proiektuaren exekuzioa kudeatzeko plangintzak', '../Proyecto/Memoria-Anexos/2_11_guias-construccion.doc',
-				         ['Norainokoaren kudeaketa', '../Proyecto/Memoria-Anexos/2_11_1_conceptos.doc'],
-				         ['Epeen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_2_directrices.doc'],
-				         ['Proiektuko kostuen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_3_materiales-soporte.doc'],
-				         ['Kalitatearen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_4_documentacion.doc'],
-				         ['Giza baliabideen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_5_plantillas.doc'],
-				         ['Komunikazioen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_6_lista-comprobacion.doc'],
-				         ['Arriskuen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_7_ejemplos.doc'],
-				         ['Erosketen kudeaketa', '../Proyecto/Memoria-Anexos/2_11_8_guias-herramientas.doc']
+                ['Proiektuaren exekuzioa kudeatzeko plangintzak', '../Proiektua/Memoria-Eranskinak/2_3_plangintzak.odt',
+				        ['Norainokoaren kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_1_norainokoak.odt'],
+				        ['Epeen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_2_epeak.odt'],
+				        ['Proiektuko kostuen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_3_kostu_kudeaketa.odt'],
+				        ['Kalitatearen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_4_kalitate_kudeaketa.odt'],
+				        ['Giza baliabideen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_5_giza_baliab_kudeaketa.odt'],
+				        ['Komunikazioen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_6_komunik_kudeaketa.odt'],
+				        ['Arriskuen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_7_arrisku_kudeaketa.odt'],
+				        ['Erosketen kudeaketa', '../Proiektua/Memoria-Eranskinak/2_3_8_erosketen_kudeaketa.odt'],
+                        ['Project Management', '../Proiektua/Memoria-Eranskinak/2_3_9_requisitos.odt',
+                            ['Iteration Plan', '../Proiektua/Memoria-Eranskinak/2_2_3_3_especificacion-sistema.odt'],
+                            ['Project Plan', '../Proiektua/Memoria-Eranskinak/2_2_3_7_requisitos-spftware.odt'],
+                            ['Risk List', '../Proiektua/Memoria-Eranskinak/2_2_3_7_requisitos-spftware.odt'],
+                            ['Work Items List', '../Proiektua/Memoria-Eranskinak/2_2_3_3_especificacion-sistema.odt']
+                        ]
                 ],
                 
-                ['Segurtasuna', '../Proyecto/Memoria-Anexos/2_10_seguridad.doc'],			
+                ['Segurtasuna', '../Proiektua/Memoria-Eranskinak/2_10_seguridad.odt'],			
 
 			],
 
-            ['Sistemaren betekizunak','../Proyecto/Memoria-Anexos/2_2_especificacion-sistema.doc',
-                ['Orokortasunak', '../Proyecto/Memoria-Anexos/2_2_1_generalidades.doc'],
-                ['Edukia', '../Proyecto/Memoria-Anexos/2_2_2_contenido.doc'],
-                ['Betekizunak', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                    ['Architecture', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                        ['Architecture notebook', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc']
-                        ],
-                    ['Development', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                        ['Build', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc'],
-                        ['Design', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc'],
-                        ['Developer Test', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc'],
-                        ['Implementation', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc']
-                        ],
-                    ['Project Management', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                        ['Iteration Plan', '../Proyecto/Memoria-Anexos/2_2_3_3_especificacion-sistema.doc'],
-                        ['Project Plan', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Risk List', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Work Items List', '../Proyecto/Memoria-Anexos/2_2_3_3_especificacion-sistema.doc']
-                        ],
-                    ['Requirements', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                        ['Glossary', '../Proyecto/Memoria-Anexos/2_2_3_4_especificacion-sup.doc'],
-                        ['Supporting Requirements Specification', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Use-Case', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Use-Case Models', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Vision', '../Proyecto/Memoria-Anexos/2_2_3_3_especificacion-sistema.doc']
-                        ],
-                    ['Test', '../Proyecto/Memoria-Anexos/2_2_3_requisitos.doc',
-                        ['Test Case', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Test Log', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc'],
-                        ['Test Script', '../Proyecto/Memoria-Anexos/2_2_3_7_requisitos-spftware.doc']
-                        ]
+            ['Sistemaren betekizunak','../Proiektua/Memoria-Eranskinak/2_2_especificacion-sistema.odt',
+                ['Orokortasunak', '../Proiektua/Memoria-Eranskinak/2_2_1_generalidades.odt'],
+                ['Edukia', '../Proiektua/Memoria-Eranskinak/2_2_2_contenido.odt'],
+                ['Requirements', '../Proiektua/Memoria-Eranskinak/2_2_3_requisitos.odt',                   
+                    ['Glossary', '../Proiektua/Memoria-Eranskinak/2_2_3_4_especificacion-sup.odt'],
+                    ['Supporting Requirements Specification', '../Proiektua/Memoria-Eranskinak/2_2_3_7_requisitos-spftware.odt'],
+                    ['Use-Case', '../Proiektua/Memoria-Eranskinak/2_2_3_7_requisitos-spftware.odt'],
+                    ['Use-Case Models', '../Proiektua/Memoria-Eranskinak/2_2_3_7_requisitos-spftware.odt'],
+                    ['Vision', '../Proiektua/Memoria-Eranskinak/2_2_3_3_especificacion-sistema.odt']
                 ]
             ],
-            ['Aurrekontua','../Proyecto/Memoria-Elaboracion/5_8_presupuesto.doc',
-					['Orokortasunak', '../Proyecto/Memoria-Elaboracion/5_8_1_generalidades.doc'],
-					['Edukia', '../Proyecto/Memoria-Elaboracion/5_8_2_contenidos.doc']
+            ['Aurrekontua','../Proiektua/Memoria-Elaboracion/5_8_presupuesto.odt',
+					['Orokortasunak', '../Proiektua/Memoria-Elaboracion/5_8_1_generalidades.odt'],
+					['Edukia', '../Proiektua/Memoria-Elaboracion/5_8_2_contenidos.odt']
             ],
-            ['Ikerlanak','../Proyecto/Memoria-Anexos/2_12_estudios-terceros.doc',
-					['Orokortasunak', '../Proyecto/Memoria-Anexos/2_12_1_generalidades.doc'],
-					['Edukia', '../Proyecto/Memoria-Anexos/2_12_2_contenido.doc']
+            ['Ikerlanak','../Proiektua/Memoria-Eranskinak/2_12_estudios-terceros.odt',
+					['Orokortasunak', '../Proiektua/Memoria-Eranskinak/2_12_1_generalidades.odt'],
+					['Edukia', '../Proiektua/Memoria-Eranskinak/2_12_2_contenido.odt']
             ]
 			
 	],			
